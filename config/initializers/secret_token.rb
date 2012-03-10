fe5f1371f12c29af5d2a1adf3e6f25ff1360dd16
Louis Simons <lousimons@gmail.com>
2012-03-10 15:36:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Connie::Application.config.secret_token = '17716a060097db76934d9b0082460f737a292a566c3844fce2499852a029e5bc8976c4253e85bd7197e00d274b6ac45b49fcc8b2d1b09a2451659659b7b77971'
