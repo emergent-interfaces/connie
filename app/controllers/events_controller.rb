c7a2072d16f50dcccb3e016330534212fd94b5c8
Louis Simons <lousimons@gmail.com>
2012-03-24 21:50:17 -0400

    Adding ability to add/edit Events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,38 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params[:event])
+
+    if @event.save
+      redirect_to @event, :notice => "Event was successfully created"
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update_attributes(params[:event])
+      redirect_to @event, :notice => 'Event was successfully updated'
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path, :notice => "Event deleted"
+  end
 end
