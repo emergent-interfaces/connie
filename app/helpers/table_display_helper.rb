d1c23e62221037dc3f6eb0eca6445cfc823216f4
superlou <lousimons@gmail.com>
2012-05-19 10:28:16 -0400

    Worthless nicities on the Events index page

---
+++ app/helpers/table_display_helper.rb
@@ -0,0 +1,28 @@
+module TableDisplayHelper
+
+  def table(collection, headers)
+    output = "<table>\n"
+    output << "\t<thead><tr>"
+    headers.each do |h|
+      output << "<td>#{h}</td>"
+    end
+    output << "</tr></thead>\n"
+
+    output << "<tbody>"
+    if collection.any?
+      collection.each do |item|
+        str = capture do
+          proc.call(item, cycle("odd","even"))
+        end
+        output << str
+      end
+    else
+      output << "\t<tr><td colspan=#{headers.count}>No records in database</td></tr>\n"
+    end
+    output << "</tbody>"
+
+    output << "</table>"
+    output.html_safe
+  end
+  
+end
