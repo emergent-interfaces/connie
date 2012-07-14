cb28d6e0eaecebf00c0a021465f3d147d4f8113d
superlou <lousimons@gmail.com>
2012-07-13 20:30:45 -0400

    Cleaning up example seeding

--- db/seeds/examples.rb
+++ db/seeds/examples.rb
@@ -2,80 +2,93 @@ require 'factory_girl'
 require File.dirname(__FILE__) + '/../../test/factories.rb'
 require 'yaml'
 
-data = YAML::load(File.open("#{Rails.root}/db/seeds/example_data.yaml"))
-
 puts "== Add Example Data =="
-
 puts "Deleting existing data"
 Convention.delete_all
 Event.delete_all
 Space.delete_all
 Reservation.delete_all
 TimeSpan.delete_all
-
 ConventionLinkable.delete_all
-
 BeScheduledRule.delete_all
 IsRelatedRule.delete_all
 DurationRule.delete_all
 RuleAssignment.delete_all
 
-puts "Creating example convention"
-# Additional models for demonstration
-anext = FactoryGirl.create :convention, name: 'AnimeNext 2010'
-bcon = FactoryGirl.create :convention, name: 'Bureaucracon IV'
+def load_convention(file_name)
+  data = YAML::load(File.open(file_name))
 
-puts "Creating example events"
-anext_events = data["anext"]["events"]
-bcon_events = data["bcon"]["events"]
+  puts "Creating example convention"
+  convention = FactoryGirl.create :convention, name: data["convention"]["name"]
+  puts "- #{convention.name}"
 
-anext_events.each do |id, event|
-  puts "- #{event["name"]}"
-  e = Event.create name: event["name"], description: event["description"]
-  Event.connection.execute("UPDATE events SET id = #{id} WHERE id = #{e.id}")
-  e = Event.find(id)
-  e.conventions << anext
-  e.save
-  start_time = DateTime.parse("#{event["start"]} EST")
-  end_time = DateTime.parse("#{event["end"]} EST")
-  e.create_time_span(:start_time => start_time, :end_time => end_time, :confidence => 2)
-end
+  convention_events = data["events"]
+  if convention_events
+    puts "Creating example events"
 
-bcon_events.each do |event|
-  FactoryGirl.create :event, name: event["name"], description: event["description"], conventions: [bcon]
-end
+    convention_events.each do |event|
+      puts "- #{event["name"]}"
+      e = Event.create name: event["name"], description: event["description"], conventions: [convention]
+      if event["start"] and event["end"]
+        start_time = DateTime.parse("#{event["start"]} EST")
+        end_time = DateTime.parse("#{event["end"]} EST")
+        e.create_time_span(:start_time => start_time, :end_time => end_time, :confidence => 2)
+      end
+    end
+  end
 
-puts "Creating example spaces"
-anext_spaces = data["anext"]["spaces"]
+  convention_spaces = data["spaces"]
+  if convention_spaces
+    puts "Creating example spaces"
 
-anext_spaces.each do |id, space|
-  s = Space.create!(:name => space["name"], :parent_id => space["parent_id"], :space_type => space["space_type"])
-  Space.connection.execute("UPDATE spaces SET id = #{id} WHERE id = #{s.id}")
-  puts "- #{s.name}"
-end
+    convention_spaces.each do |space|
+      s = Space.create!(:name => space["name"],
+                        :parent => Space.find_by_name(space["parent"]),
+                        :space_type => space["space_type"],
+                        :conventions => [convention])
+      puts "- #{s.name}"
+    end
+  end
 
-puts "Creating example reservations"
-anext_reservations = data["anext"]["reservations"]
 
-anext_reservations.each do |reservation|
-  e = Event.find(reservation["event"])
-  reservable_class = reservation["reservable_type"].constantize
-  reservable_id = reservation["reservable_id"]
-  r = e.reservations.create(:reservable => reservable_class.find(reservable_id))
+  convention_reservations = data["reservations"]
+  if convention_reservations
+    puts "Creating example reservations"
 
-  puts "- #{e.name} reserves #{r.reservable.name}"
-end
+    data["reservations"].each do |reservation|
+      e = Event.find_by_name(reservation["event"])
+      reservable_class = reservation["reservable_type"].constantize
+      reservable = reservation["reservable"]
+      r = e.reservations.create(:reservable => reservable_class.find_by_name(reservable))
 
-puts "Creating example rules"
+      puts "- #{e.name} reserves #{r.reservable.name}"
+    end
+  end
 
-data["anext"]["rules"].each do |rule|
-  case rule["type"]
-    when "IsRelatedRule"
-      event = Event.find(rule["event_id"])
-      related_event = Event.find(rule["related_event_id"])
-      rule = IsRelatedRule.create(:related_event => related_event, :relation => rule["relation"])
-      RuleAssignment.create(:event => event, :rule => rule)
 
-      puts "- #{event.name} #{rule["relation"]} #{related_event.name}"
+  convention_rules = data["rules"]
+  if convention_rules
+    puts "Creating example rules"
+
+    data["rules"].each do |rule|
+      case rule["type"]
+        when "IsRelatedRule"
+          event = Event.find_by_name(rule["event"])
+          related_event = Event.find_by_name(rule["related_event"])
+          rule = IsRelatedRule.create(:related_event => related_event, :relation => rule["relation"])
+          RuleAssignment.create(:event => event, :rule => rule)
+
+          puts "- #{event.name} #{rule["relation"]} #{related_event.name}"
+      end
+    end
   end
 end
+
+load_convention("#{Rails.root}/db/seeds/anext_data.yaml")
+load_convention("#{Rails.root}/db/seeds/bcon_data.yaml")
+
+
+
+
+
+
