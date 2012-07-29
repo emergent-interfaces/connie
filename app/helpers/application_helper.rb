9ef73c07ee0a91ac54074a149eb08028e022b88b
superlou <lousimons@gmail.com>
2012-07-28 21:56:34 -0400

    Refactoring icon links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
+  def edit_link_text(text=nil)
+    link_text = content_tag(:i, "", :class=>"icon-pencil")
+    link_text += " #{text}" if text
+    link_text.html_safe
+  end
 
+  def delete_link_text(text=nil)
+    link_text = content_tag(:i, "", :class=>"icon-remove")
+    link_text += " #{text}" if text
+    link_text.html_safe
+  end
+
+  def add_link_text(text=nil)
+    link_text = content_tag(:i, "", :class=>"icon-plus")
+    link_text += " #{text}" if text
+    link_text.html_safe
+  end
 end
