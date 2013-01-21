9050cdf9edc8a33435e2ef10308bdbfb96f94038
Louis Simons <lousimons@gmail.com>
2013-01-20 20:22:33 -0500

    Fixing running events chart

--- app/assets/javascripts/rundown_chart.js
+++ app/assets/javascripts/rundown_chart.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     _.each(chart, function(schedule) {
         $.getJSON($(schedule).data('url'),
             function(data) {
-                console.log(data);
+                //console.log(data);
                 draw_rundown_chart(schedule,data);
             }
         );
